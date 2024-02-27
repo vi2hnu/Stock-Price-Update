e8ffd3a7e7f2b48520e6d289fd765a7cda436d86
vi2hnu <130527742+vi2hnu@users.noreply.github.com>
2024-02-27 18:54:37 +0530

    Update main.py
    now the output is being saved into a file for later use

--- main.py
+++ main.py
@@ -2,27 +2,30 @@ from bs4 import BeautifulSoup
 import requests
 import threading
 
-soup = BeautifulSoup(requests.get('https://www.screener.in/company/HCLTECH/consolidated/').text,'lxml')
+soup = BeautifulSoup(requests.get('https://www.screener.in/company/HCLTECH/consolidated/').text, 'lxml')
 stack = []
 stack.append(0)
-stock_price = soup.find_all('span',class_='number')
-name = soup.find_all('span',class_='name')
+stock_price = soup.find_all('span', class_='number')
+name = soup.find_all('span', class_='name')
+file_path = 'C:\\Users\\Vishnu\\Downloads\\output.txt'
 def repeat_print():
-    soup = BeautifulSoup(requests.get('https://www.screener.in/company/HCLTECH/consolidated/').text,'lxml')
-    stock_price = soup.find_all('span',class_='number')
-    prev = stack[-1]
-    print("Current stock price is", stock_price[1].text)
-    stack.append(int(stock_price[1].text.replace(',', '')))
-    if stack[-1] > prev:
-        print("Stock price has gone up, might be a good time to sell\n")
-    elif stack[-1]<prev:
-        print("Stock price has gone down, might be a good time to buy\n")
-    else:
-        print("No change in stock price\n")
+    with open(file_path, 'a') as file:
+        soup = BeautifulSoup(requests.get('https://www.screener.in/company/HCLTECH/consolidated/').text, 'lxml')
+        stock_price = soup.find_all('span', class_='number')
+        prev = stack[-1]
+        file.write("Current stock price is " + str(stock_price[1].text) + "\n")
+        stack.append(int(stock_price[1].text.replace(',', '')))
+        if stack[-1] > prev:
+            file.write("Stock price has gone up, might be a good time to sell\n")
+            file.write("\n")
+        elif stack[-1] < prev:
+            file.write("Stock price has gone down, might be a good time to buy\n")
+            file.write("\n")
+        else:
+            file.write("No change in stock price\n")
+            file.write("\n")
 
-    
-    timer = threading.Timer(150, repeat_print)
+    timer = threading.Timer(10, repeat_print)
     timer.start()
 
-print("HCL Technologies Ltd currently holds mrket cap of",stock_price[0].text,"\n")
 repeat_print()
